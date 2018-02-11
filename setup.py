7228ac44c42a8b741b2235330d7357a20f1e17a6
Dawid Wolski <dwolski@macrosystem.pl>
2018-02-11 21:00:16 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='kontrib',
+      version='0.1',
+      description='Make contribution easy',
+      author='Dawid Wolski',
+      author_email='merito123@gmail.com',
+      url='https://github.com/merito/kontrib',
+     )
